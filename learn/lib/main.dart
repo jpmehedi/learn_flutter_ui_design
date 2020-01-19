0aec9246bf39b486da6eb673102a9974692fcd96
DESKTOP-0B563Q2\engrm <engrmhnirob47@gmail.com>
2020-01-19 21:14:31 +0600

    learn listview

--- learn/lib/main.dart
+++ learn/lib/main.dart
@@ -14,77 +14,320 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text("Home"),
       ),
-      body: Column(
+      body: ListView(
         children: <Widget>[
-          Row(
-            children: <Widget>[
-              Container(
-                height: 100,
-                width: 100,
-                padding: EdgeInsets.all(15),
-                margin: EdgeInsets.fromLTRB(15, 30, 0, 0),
-                // color: Colors.purple,
-                decoration: BoxDecoration(
-                  color: Colors.purple,
-                  borderRadius: BorderRadius.circular(15),
-                ),
-                child: Center(
-                  child: Text(
-                    'Hello Bangladesh',
-                    textAlign: TextAlign.center,
+          Column(
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Container(
+                  height: 100,
+                  width: 100,
+                  padding: EdgeInsets.all(15),
+                  margin: EdgeInsets.fromLTRB(15, 30, 0, 0),
+                  // color: Colors.purple,
+                  decoration: BoxDecoration(
+                    color: Colors.purple,
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  child: Center(
+                    child: Text(
+                      'Hello Bangladesh',
+                      textAlign: TextAlign.center,
+                    ),
                   ),
                 ),
-              ),
-              Container(
-                //appliying circuler image
-                height: 100,
-                width: 100,
-                margin: EdgeInsets.fromLTRB(15, 30, 0, 0),
-                decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    image: DecorationImage(
-                      fit: BoxFit.cover,
-                      image: NetworkImage(
-                          'https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80'),
-                    )),
-              ),
-            ],
-          ),
-          Row(
-            children: <Widget>[
-              Container(
-                height: 100,
-                width: 100,
-                padding: EdgeInsets.all(15),
-                margin: EdgeInsets.fromLTRB(15, 30, 0, 0),
-                // color: Colors.purple,
-                decoration: BoxDecoration(
-                  color: Colors.purple,
-                  borderRadius: BorderRadius.circular(15),
+                Container(
+                  //appliying circuler image
+                  height: 100,
+                  width: 100,
+                  margin: EdgeInsets.fromLTRB(15, 30, 0, 0),
+                  decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      image: DecorationImage(
+                        fit: BoxFit.cover,
+                        image: NetworkImage(
+                            'https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80'),
+                      )),
                 ),
-                child: Center(
-                  child: Text(
-                    'Hello Bangladesh',
-                    textAlign: TextAlign.center,
+              ],
+            ),
+                      Row(
+              children: <Widget>[
+                Container(
+                  height: 100,
+                  width: 100,
+                  padding: EdgeInsets.all(15),
+                  margin: EdgeInsets.fromLTRB(15, 30, 0, 0),
+                  // color: Colors.purple,
+                  decoration: BoxDecoration(
+                    color: Colors.purple,
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  child: Center(
+                    child: Text(
+                      'Hello Bangladesh',
+                      textAlign: TextAlign.center,
+                    ),
                   ),
                 ),
-              ),
-              Container(
-                //appliying circuler image
-                height: 100,
-                width: 100,
-                margin: EdgeInsets.fromLTRB(15, 30, 0, 0),
-                decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    image: DecorationImage(
-                      fit: BoxFit.cover,
-                      image: NetworkImage(
-                          'https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80'),
-                    )),
-              ),
-            ],
-          ),
+                Container(
+                  //appliying circuler image
+                  height: 100,
+                  width: 100,
+                  margin: EdgeInsets.fromLTRB(15, 30, 0, 0),
+                  decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      image: DecorationImage(
+                        fit: BoxFit.cover,
+                        image: NetworkImage(
+                            'https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80'),
+                      )),
+                ),
+              ],
+            ),
+                      Row(
+              children: <Widget>[
+                Container(
+                  height: 100,
+                  width: 100,
+                  padding: EdgeInsets.all(15),
+                  margin: EdgeInsets.fromLTRB(15, 30, 0, 0),
+                  // color: Colors.purple,
+                  decoration: BoxDecoration(
+                    color: Colors.purple,
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  child: Center(
+                    child: Text(
+                      'Hello Bangladesh',
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ),
+                Container(
+                  //appliying circuler image
+                  height: 100,
+                  width: 100,
+                  margin: EdgeInsets.fromLTRB(15, 30, 0, 0),
+                  decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      image: DecorationImage(
+                        fit: BoxFit.cover,
+                        image: NetworkImage(
+                            'https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80'),
+                      )),
+                ),
+              ],
+            ),
+                      Row(
+              children: <Widget>[
+                Container(
+                  height: 100,
+                  width: 100,
+                  padding: EdgeInsets.all(15),
+                  margin: EdgeInsets.fromLTRB(15, 30, 0, 0),
+                  // color: Colors.purple,
+                  decoration: BoxDecoration(
+                    color: Colors.purple,
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  child: Center(
+                    child: Text(
+                      'Hello Bangladesh',
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ),
+                Container(
+                  //appliying circuler image
+                  height: 100,
+                  width: 100,
+                  margin: EdgeInsets.fromLTRB(15, 30, 0, 0),
+                  decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      image: DecorationImage(
+                        fit: BoxFit.cover,
+                        image: NetworkImage(
+                            'https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80'),
+                      )),
+                ),
+              ],
+            ),
+                      Row(
+              children: <Widget>[
+                Container(
+                  height: 100,
+                  width: 100,
+                  padding: EdgeInsets.all(15),
+                  margin: EdgeInsets.fromLTRB(15, 30, 0, 0),
+                  // color: Colors.purple,
+                  decoration: BoxDecoration(
+                    color: Colors.purple,
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  child: Center(
+                    child: Text(
+                      'Hello Bangladesh',
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ),
+                Container(
+                  //appliying circuler image
+                  height: 100,
+                  width: 100,
+                  margin: EdgeInsets.fromLTRB(15, 30, 0, 0),
+                  decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      image: DecorationImage(
+                        fit: BoxFit.cover,
+                        image: NetworkImage(
+                            'https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80'),
+                      )),
+                ),
+              ],
+            ),
+                      Row(
+              children: <Widget>[
+                Container(
+                  height: 100,
+                  width: 100,
+                  padding: EdgeInsets.all(15),
+                  margin: EdgeInsets.fromLTRB(15, 30, 0, 0),
+                  // color: Colors.purple,
+                  decoration: BoxDecoration(
+                    color: Colors.purple,
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  child: Center(
+                    child: Text(
+                      'Hello Bangladesh',
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ),
+                Container(
+                  //appliying circuler image
+                  height: 100,
+                  width: 100,
+                  margin: EdgeInsets.fromLTRB(15, 30, 0, 0),
+                  decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      image: DecorationImage(
+                        fit: BoxFit.cover,
+                        image: NetworkImage(
+                            'https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80'),
+                      )),
+                ),
+              ],
+            ),
+                      Row(
+              children: <Widget>[
+                Container(
+                  height: 100,
+                  width: 100,
+                  padding: EdgeInsets.all(15),
+                  margin: EdgeInsets.fromLTRB(15, 30, 0, 0),
+                  // color: Colors.purple,
+                  decoration: BoxDecoration(
+                    color: Colors.purple,
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  child: Center(
+                    child: Text(
+                      'Hello Bangladesh',
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ),
+                Container(
+                  //appliying circuler image
+                  height: 100,
+                  width: 100,
+                  margin: EdgeInsets.fromLTRB(15, 30, 0, 0),
+                  decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      image: DecorationImage(
+                        fit: BoxFit.cover,
+                        image: NetworkImage(
+                            'https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80'),
+                      )),
+                ),
+              ],
+            ),
+                      Row(
+              children: <Widget>[
+                Container(
+                  height: 100,
+                  width: 100,
+                  padding: EdgeInsets.all(15),
+                  margin: EdgeInsets.fromLTRB(15, 30, 0, 0),
+                  // color: Colors.purple,
+                  decoration: BoxDecoration(
+                    color: Colors.purple,
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  child: Center(
+                    child: Text(
+                      'Hello Bangladesh',
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ),
+                Container(
+                  //appliying circuler image
+                  height: 100,
+                  width: 100,
+                  margin: EdgeInsets.fromLTRB(15, 30, 0, 0),
+                  decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      image: DecorationImage(
+                        fit: BoxFit.cover,
+                        image: NetworkImage(
+                            'https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80'),
+                      )),
+                ),
+              ],
+            ),
+                      Row(
+              children: <Widget>[
+                Container(
+                  height: 100,
+                  width: 100,
+                  padding: EdgeInsets.all(15),
+                  margin: EdgeInsets.fromLTRB(15, 30, 0, 0),
+                  // color: Colors.purple,
+                  decoration: BoxDecoration(
+                    color: Colors.purple,
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  child: Center(
+                    child: Text(
+                      'Hello Bangladesh',
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ),
+                Container(
+                  //appliying circuler image
+                  height: 100,
+                  width: 100,
+                  margin: EdgeInsets.fromLTRB(15, 30, 0, 0),
+                  decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      image: DecorationImage(
+                        fit: BoxFit.cover,
+                        image: NetworkImage(
+                            'https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80'),
+                      )),
+                ),
+              ],
+            ),
+          ],
+        ),
         ],
+         
       ),
     );
   }
