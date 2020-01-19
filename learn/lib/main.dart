abf25202a5dab8dbc42a614a8f7d8b1429222585
DESKTOP-0B563Q2\engrm <engrmhnirob47@gmail.com>
2020-01-19 19:38:02 +0600

    learn using paddin and text

--- learn/lib/main.dart
+++ learn/lib/main.dart
@@ -1,4 +1,28 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main(){
+  runApp(
+    MaterialApp(
+      title: 'My APP',
+      home: HomePage(),
+    )
+  );
+ 
+}
+
+class HomePage extends StatelessWidget{
+  @override 
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Home"),),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(10, 30, 10,0),
+        child: Text(
+          'The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog. The quick brown fox jumps  over the lazy dog. The quick broxn fox jumps over the lazy dog',textAlign:TextAlign.justify, style:TextStyle(color:Colors.green, fontSize:18, fontWeight: FontWeight.bold, fontStyle: FontStyle.normal,fontFamily: 'arial')
+        ),
+      ),
+    );
+   
+  }
+  }
 
